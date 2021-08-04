e3fa7dd99ba999bb4487eab34a4a069ea801772d
Shyam Murali <shyammurali23@gmail.com>
2021-08-04 22:59:13 +0530

    try catch optimization

--- lib/homepage.dart
+++ lib/homepage.dart
@@ -39,9 +39,9 @@ class _HomePageState extends State<HomePage> {
 
 
 
-    var url = Uri.parse('https://a9691734c54993.localhost.run/');
+    var url = Uri.parse('https://imagecaption2021.herokuapp.com/');
     // https://imagecaption2021.herokuapp.com/
-    // https://a9691734c54993.localhost.run
+    // https://a9691734c54993.localhost.run/
 
     Map <String, dynamic> requestpayload =  {
       'image': base64Image
@@ -77,6 +77,9 @@ class _HomePageState extends State<HomePage> {
         });
       }
     }catch(e){
+      setState(() {
+        caption = "Timed out";
+      });
       print(e.toString());
     }
 
